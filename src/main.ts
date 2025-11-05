d7feaf31d7266d61183d0e08241bc3f5eb22576d
Hasibul Hasan Shanto <mdhasibulhasan165@gmail.com>
2025-11-05 14:58:59 +0600

    Updated CORS configuration with frontend URL

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin:
+      process.env.FRONTEND_URL ||
+      "http://localhost:3000" ||
+      "https://sso-frontend-livid.vercel.app/",
     credentials: true,
   });
   await app.listen(process.env.PORT);
